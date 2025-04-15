e599d0ce6a1d12c0b670d4b4fba7f8c532d81b78
epsilon11101 <28758777+epsilon11101@users.noreply.github.com>
2025-04-15 15:35:15 -0600

    feat(components): redux , rtk , configs

---
+++ src/lib/store.ts
@@ -0,0 +1,43 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "./ssr-safe-storage";
+import { catApi } from "@/api";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: [],
+};
+
+const rootReducer = combineReducers({
+  [catApi.reducerPath]: catApi.reducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const makeStore = () =>
+  configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+      }).concat(catApi.middleware),
+  });
+
+export const store = makeStore();
+export const persistor = persistStore(store);
+
+export type AppStore = typeof store;
+export type RootState = ReturnType<AppStore["getState"]>;
+export type AppDispatch = AppStore["dispatch"];
